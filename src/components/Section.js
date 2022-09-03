fb2104692fb34dde75196dd957063a3e3dd46d59
vdp-eve <76458770+vdp-eve@users.noreply.github.com>
2022-09-02 23:27:56 -0500

    Add files via upload

---
+++ src/components/Section.js
@@ -0,0 +1,24 @@
+class Section {
+ 
+
+  constructor({ items, renderer }, container) { 
+    this._renderedItems = items; 
+    this._renderer = renderer; 
+    this._container = container; 
+  } 
+
+  addItem(element) { 
+    this._container.prepend(element); 
+  } 
+
+  renderItems() { 
+    this._renderedItems.forEach((item) => { 
+      this._renderer(item); 
+    }); 
+  } 
+} 
+
+
+
+export default Section;
+ 
