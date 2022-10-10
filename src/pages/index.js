f199d10e33e90c18f65d34eede2821ccf132627a
vdp-eve <76458770+vdp-eve@users.noreply.github.com>
2022-10-09 22:41:30 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,11 @@ import {
   validationSettings,
 } from "../utils/constants.js";
 
+const api = new Api ({
+baseUrl: "https://around.nomoreparties.co/v1/group-12",
+authToken: "099989f7-9742-46ef-99f8-aa8e6872a6c3 "
+});
+
 // Card Functions
 
 const createCard = (cardData) => { 
