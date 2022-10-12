af3ef0c45304a7c7b8d89adf0d297d0462e7af30
vdp-eve <76458770+vdp-eve@users.noreply.github.com>
2022-10-11 23:49:09 -0500

    Add files via upload

---
+++ src/components/UserInfo.js
@@ -0,0 +1,15 @@
+export default class UserInfo { 
+  constructor({ nameSelector, careerSelector }) {
+    this._name = document.querySelector(nameSelector); 
+    this._career = document.querySelector(careerSelector); 
+  } 
+
+  getUserInfo() { 
+    return { name: this._name.textContent, occupation: this._career.textContent }; 
+  } 
+
+  setUserInfo(data) { 
+    this._name.textContent = data.names; 
+    this._career.textContent = data.occupation; 
+  } 
+} 
