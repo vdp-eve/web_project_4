e0ae158fbfb27480afbb49c2a830cb52632e3c5f
vdp-eve <76458770+vdp-eve@users.noreply.github.com>
2022-10-12 07:58:57 -0500

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -7,42 +7,37 @@ class Api {
     }
 
 
-
 // GET https://around.nomoreparties.co/v1/group-12/cards
 
-    getCardList() {
-      return fetch(`${this._baseUrl}/cards`, {
-  headers: {
-    authorization: "099989f7-9742-46ef-99f8-aa8e6872a6c3"
-  }
-})
-  .then(res => res.json())
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err); // log the error to the console
-  });
+    async getCardList() {
+      try {
+        const res = await fetch(`${this._baseUrl}/cards`, {
+          headers: {
+            authorization: "099989f7-9742-46ef-99f8-aa8e6872a6c3"
+          }
+        });
+        const result_1 = await res.json();
+        console.log(result_1);
+      } catch (err) {
+        console.log(err); // log the error to the console
+      }
 }
 
 // GET https://around.nomoreparties.co/v1/group-12/users/me
- getUserInfo() {
-  return fetch(`${this._baseUrl}/users/me`, {
-    headers: {
-      authorization: "099989f7-9742-46ef-99f8-aa8e6872a6c3"
-    }
-  })
-    .then(res => res.json())
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => {
-      console.log(err); // log the error to the console
-    });
+ async getUserInfo() {
+  try {
+     const res = await fetch(`${this._baseUrl}/users/me`, {
+       headers: {
+         authorization: "099989f7-9742-46ef-99f8-aa8e6872a6c3"
+       }
+     });
+     const result_1 = await res.json();
+     console.log(result_1);
+   } catch (err) {
+     console.log(err); // log the error to the console
+   }
 }
 
-
-// getAppInfo { }
 
 
 // POST https://around.nomoreparties.co/v1/group-12/cards
@@ -72,3 +67,4 @@ class Api {
 };
 
 export default Api;
+
